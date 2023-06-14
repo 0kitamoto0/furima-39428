58eb5ef20768d3358f8f83b156a8944ff94a9568
ttk990 <tatuki990715@gmail.com>
2023-06-14 17:38:56 +0900

    数字とアルファベットが含まれるパスワードを生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字混合で入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :date_of_birth, presence: true
