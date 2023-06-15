e6ebe5ca3cf673b34a68a797e8c14765dc21a640
ttk990 <tatuki990715@gmail.com>
2023-06-15 15:38:48 +0900

    コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @tweet = Tweet.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:title,:description,:price,:category_id,:condition_id,:shipping_fee_id,:prefecture_id,:days_to_ship_id)
   end
 end
