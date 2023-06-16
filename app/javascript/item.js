12c15588776ce50da153e817dd5e7a941edc090c
ttk990 <tatuki990715@gmail.com>
2023-06-16 16:32:04 +0900

    JS手数料実装

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,5 +1,15 @@
-function post (){
-  console.log("非同期投稿を実装");
-};
+function calculateProfit() {
+  const amountInput = document.getElementById('item-price');
+  const result90 = document.getElementById('profit');
+  const result10 = document.getElementById('add-tax-price');
 
-window.addEventListener('turbo:load', post);
+  amountInput.addEventListener("keyup", () => {
+    const amount = parseFloat(amountInput.value);
+    const profit90 = Math.floor(amount * 0.9); // 金額の90%を切り捨てて計算
+    const profit10 = Math.floor(amount * 0.1); // 金額の10%を切り捨てて計算
+    result90.innerHTML = `${profit90}`; // price-contentに金額の90%を表示
+    result10.innerHTML = `${profit10}`; // add-tax-priceに金額の10%を表示
+  });
+}
+
+window.addEventListener('turbo:load', calculateProfit);
