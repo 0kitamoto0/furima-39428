2ec4d27966c60c717935ceb2d0c19f889efd5c01
ttk990 <tatuki990715@gmail.com>
2023-06-20 16:32:23 +0900

    DRYの観点で修正02

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :require_login, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
