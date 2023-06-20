ea9802b1bb160f783af8a0f4dbf675ee7bece514
ttk990 <tatuki990715@gmail.com>
2023-06-20 16:30:38 +0900

    DRYの観点で修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,23 +20,27 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     return if current_user == @item.user
 
     redirect_to root_path, alert: '編集権限がありません'
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
 
   def require_login
@@ -45,10 +49,7 @@ class ItemsController < ApplicationController
     redirect_to user_session_path, alert: 'ログインが必要です'
   end
 
-  private
-
-  def item_params
-    params.require(:item).permit(:title, :description, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
-                                 :days_to_ship_id, :image).merge(user_id: current_user.id)
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
