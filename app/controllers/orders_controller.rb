2d359d48e86990544ae1be6d2dd998f4679868f0
ttk990 <tatuki990715@gmail.com>
2023-06-22 23:28:44 +0900

    購入情報保存実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,31 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :new, :create]
+  
   def index
-    @order = Order.new
+    @order_order_detail = OrderOrderDetail.new
+  end
+
+  def new
+    @order_order_detail = OrderOrderDetail.new
   end
 
   def create
+    @order_order_detail = OrderOrderDetail.new(order_params)
+    if @order_order_detail.valid?
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_order_detail).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
---
+++ app/models/order_order_detail.rb
@@ -0,0 +1,18 @@
+class OrderOrderDetail
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁ハイフン4桁」の形式で入力してください" }
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数字で入力してください" }
+    validates :city
+    validates :street_address
+  end
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    OrderDetail.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
