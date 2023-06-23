e0fb472bcfa2dc13f4b35fb80093bbd5ddb5230d
ttk990 <tatuki990715@gmail.com>
2023-06-23 14:55:52 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_one :order
 
   validates :title, :description, presence: true
   validates :image, presence: { message: "can't be blank" }
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :prefecture
   belongs_to :user
+  belongs_to :item
   has_one :order_detail
 end
