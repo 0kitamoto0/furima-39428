efc154762df350e9937db680d80877d335986e7f
ttk990 <134995279+0kitamoto0@users.noreply.github.com>
2023-06-27 18:08:05 +0900

    金額表示修正

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -5,8 +5,8 @@ function calculateProfit() {
 
   amountInput.addEventListener("keyup", () => {
     const amount = parseFloat(amountInput.value);
-    const profit90 = Math.floor(amount * 0.9); // 金額の90%を切り捨てて計算
     const profit10 = Math.floor(amount * 0.1); // 金額の10%を切り捨てて計算
+    const profit90 = amount - profit10; // 金額の90%を切り捨てて計算
     result90.innerHTML = `${profit90}`; // price-contentに金額の90%を表示
     result10.innerHTML = `${profit10}`; // add-tax-priceに金額の10%を表示
   });
