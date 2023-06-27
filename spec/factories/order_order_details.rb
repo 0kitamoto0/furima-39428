d323f7fc6eea460ab6bb9dda87e69b2d2e8df034
ttk990 <134995279+0kitamoto0@users.noreply.github.com>
2023-06-27 13:40:33 +0900

    テストコード02

--- spec/factories/order_order_details.rb
+++ spec/factories/order_order_details.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :order_order_detail do
-    association :user
-    association :item
     token {"tok_abcdefghijk00000000000000000"}
-    postal_code { Faker::Address.postcode.gsub(/\s+|-/, '') }
+    postal_code { "123-4567" }
     prefecture_id { Faker::Number.between(from: 2, to: 47) }
     phone_number { Faker::Number.unique.number(digits: Faker::Number.between(from: 10, to: 11)) }
     city { Faker::Address.city }
